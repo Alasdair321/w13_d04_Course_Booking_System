6c2b6aa4271e8e7e840f6876acb09bc030456953
Alasdair321 <a.g.carstairs@outlook.com>
2019-05-23 11:41:58 +0100

    test

--- CourseBookingSystem/src/main/java/com/codeclan/CourseBookingSystem/repositories/BookingRepository/BookingRepository.java
+++ CourseBookingSystem/src/main/java/com/codeclan/CourseBookingSystem/repositories/BookingRepository/BookingRepository.java
@@ -1,7 +1,6 @@
 package com.codeclan.CourseBookingSystem.repositories.BookingRepository;
 
 import com.codeclan.CourseBookingSystem.models.Booking;
-import com.codeclan.CourseBookingSystem.projections.EmbedBookingsInCourse;
 import com.codeclan.CourseBookingSystem.projections.EmbedCoursesInBookings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
